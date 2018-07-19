417c7c503aef315688b0c41da02954fc3b30846b
Dwi Wicaksono <dwiwicaksono@Ndiw-MacBook-Pro.local>
2018-07-20 00:01:39 +0700

    Tugas week4

---
+++ ex18.js
@@ -0,0 +1,18 @@
+function kaliTerusRekursif(angka) {
+  var angkaStr = String(angka)
+  var hasil = 1
+  for (var i = 0; i < angkaStr.length; i++){
+    hasil = hasil * Number(angkaStr[i])
+  }
+  if (String(hasil).length === 1){
+    return Number(hasil)
+  }
+  return kaliTerusRekursif(Number(hasil))
+}
+
+// TEST CASES
+console.log(kaliTerusRekursif(66)); // 8
+console.log(kaliTerusRekursif(3)); // 3
+console.log(kaliTerusRekursif(24)); // 8
+console.log(kaliTerusRekursif(654)); // 0
+console.log(kaliTerusRekursif(1231)); // 6
