38fed847fc5aef7823b0b8c5c9e205e1bc305340
Dwi Wicaksono <dwiwicaksono@Ndiw-MacBook-Pro.local>
2018-07-20 18:59:23 +0700

    revisi ex18

--- ex18.js
+++ ex18.js
@@ -1,15 +1,23 @@
-function kaliTerusRekursif(angka) {
-  var angkaStr = String(angka)
-  var hasil = 1
-  for (var i = 0; i < angkaStr.length; i++){
-    hasil = hasil * Number(angkaStr[i])
-  }
-  if (String(hasil).length === 1){
-    return Number(hasil)
-  }
-  return kaliTerusRekursif(Number(hasil))
-}
+// function kaliTerusRekursif(angka) {
+//   var angkaStr = String(angka)
+//   var hasil = 1
+//   for (var i = 0; i < angkaStr.length; i++){
+//     hasil = hasil * Number(angkaStr[i])
+//   }
+//   if (String(hasil).length === 1){
+//     return Number(hasil)
+//   }
+//   return kaliTerusRekursif(Number(hasil))
+// }
 
+
+// cara pendek
+function kaliTerusRekursif(angka) {
+  if (String(angka).length  === 1 ){
+return angka
+  }
+  return  kaliTerusRekursif(Number(String(angka)[0]) * kaliTerusRekursif(Number(String(angka).slice(1))))
+}
 // TEST CASES
 console.log(kaliTerusRekursif(66)); // 8
 console.log(kaliTerusRekursif(3)); // 3
